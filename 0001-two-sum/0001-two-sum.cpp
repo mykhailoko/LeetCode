23b551dfe1c7133517596685b6b0f705aaf3c695
mykhailoko <145272215+mykhailoko@users.noreply.github.com>
2024-06-29 20:56:34 +0300

    Create 0001-two-sum.cpp

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       for (int i=0; i<nums.size(); i++){
+            for (int j=1; j<nums.size(); j++){
+                if ((nums[i] + nums[j] == target) and (j != i)){
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
